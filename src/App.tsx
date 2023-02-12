d5b97f4fcf9e39f0ea1ce708d7458a34d496aa42
Carlo Martinucci <carlo.martinucci@gmail.com>
2023-02-12 13:58:40 +0100

    feat: improve navigation, fix print from mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
 
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
@@ -24,48 +25,62 @@ function App() {
 
   const componentRef = useRef(null);
   const handlePrint = useReactToPrint({
-    content: () => componentRef.current,
-    onAfterPrint: () => {
-      dispatch(increment())
-      dispatch(reset())
-    }
+    content: () => componentRef.current
   });
 
   return (
     <>
-    <Navbar bg="dark" variant="dark">
-      <Container fluid>
-         <Navbar.Brand>Ordine #{count}</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <div className="d-print-none">
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand onClick={() => setNavigation("order")}>Ordine #{count}</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link active={navigation === "recap"} disabled={navigation === "order"} onClick={() => setNavigation("recap")}>&gt; Riepilogo</Nav.Link>
+            <Nav.Link active={navigation === "pay"} disabled={["order", "recap"].includes(navigation)} onClick={() => setNavigation("pay")}>&gt; Paga</Nav.Link>
+            <Nav.Link active={navigation === "done"} disabled={["order", "recap", "pay"].includes(navigation)} onClick={() => setNavigation("done")}>&gt; Fatto</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-    {
-      navigation === "order" ?
-        <main>
-          <Order />
+      {
+        navigation === "order" ?
+          <main>
+            <Order />
 
-          <Container fluid className="text-center">
-            <Button size="lg" onClick={() => { setNavigation("recap") }}>Procedi al riepilogo</Button>
-          </Container>
-        </main> :
-      navigation === "recap" ?
-        <main>
-          <RecapOrder />
+            <Container fluid className="text-center">
+              <Button size="lg" onClick={() => { setNavigation("recap") }}>Procedi al riepilogo</Button>
+            </Container>
+          </main> :
+        navigation === "recap" ?
+          <main>
+            <RecapOrder />
 
-          <Container fluid className="text-center">
-            <Button style={{ marginRight: 20 }}variant="secondary" size="lg" onClick={() => { setNavigation("order") }}>Torna all'ordine</Button>
-            <Button size="lg" onClick={() => { setNavigation("pay") }}>Procedi al pagamento</Button>
-          </Container>
-        </main> :
-        <main>
-          <Total />
+            <Container fluid className="text-center">
+              <Button style={{ marginRight: 20 }}variant="secondary" size="lg" onClick={() => { setNavigation("order") }}>Torna indietro</Button>
+              <Button size="lg" onClick={() => { setNavigation("pay") }}>Procedi al pagamento</Button>
+            </Container>
+          </main> :
+        navigation === "pay" ?
+          <main>
+            <Total />
 
-          <Container style={{paddingTop: 30}} fluid className="text-center">
-            <Button style={{ marginRight: 20 }}variant="secondary" size="lg" onClick={() => { setNavigation("order") }}>Torna all'ordine</Button>
-            <Button size="lg" onClick={() => { handlePrint(); setNavigation("order") }}>Conferma e stampa</Button>
-          </Container>
-        </main>
-    }
+            <Container style={{paddingTop: 30}} fluid className="text-center">
+              <Button style={{ marginRight: 20 }}variant="secondary" size="lg" onClick={() => { setNavigation("recap") }}>Torna indietro</Button>
+              <Button size="lg" onClick={() => { handlePrint(); setNavigation("done") }}>Conferma e stampa</Button>
+            </Container>
+          </main> :
+        navigation === "done" ?
+          <main>
+            <Container fluid className="text-center">
+              <p style={{ marginTop: 10 }}>Ordine #{count} concluso! Se c'è stato qualche problema, torna indietro o stampa di nuovo. Altrimenti, procedi con un nuovo ordine.</p>
+              <Button style={{ marginBottom: 10 }} variant="secondary" size="lg" onClick={() => { setNavigation("pay") }}>Torna indietro</Button><br/>
+              <Button style={{ marginBottom: 10 }} variant="secondary" size="lg" onClick={() => { handlePrint() }}>Stampa di nuovo</Button><br/>
+              <Button size="lg" onClick={() => { dispatch(increment()); dispatch(reset()); setNavigation("order") }}>Nuovo ordine</Button>
+            </Container>
+          </main> :
+          <div>Questo non dovrebbe mai succedere. Se vedi questa schermata, ricordati la parola "{navigation}" e aggiorna la pagina. Grazie.</div>
+      }
+    </div>
 
     <div className="d-none d-print-block" ref={componentRef}>
       <PrintableOrder />
